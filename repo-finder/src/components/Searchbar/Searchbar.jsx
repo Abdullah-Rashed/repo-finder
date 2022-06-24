cf2fbb86ac42c0d1a51b8d4a5a393031ec570914
Abdullah-Rashed <abdullahrashed2000@gmail.com>
2022-06-24 18:35:37 +0200

    initial_working_prototype_commit

--- repo-finder/src/components/Searchbar/Searchbar.jsx
+++ repo-finder/src/components/Searchbar/Searchbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import "./searchbar.css"
 import search from "../images/search.svg"
-const Searchbar = () => {
+const Searchbar = ({ handleSearch, text, setText }) => {
   return (
     <div className='container'>
       <div className="searchbar-container">
         <img className='search-icon' src={search} alt="" />
-        <input className='searchbar' type="text" placeholder='search for repos' />
+        <input className='searchbar'
+          type="text"
+          placeholder='search for repos'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onkey
+        />
       </div>
+      <button onClick={handleSearch}>add</button>
     </div>
   );
 }
